c40faf999153e5622edfbffdd0608f0ab51e0c17
Rodrigo Bulhões <rodrigobulhoes03@gmail.com>
2026-03-10 11:58:59 -0100

    Implementation of the database connection.

---
+++ config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "127.0.0.1";
+$port = "3306";
+$dbname = "Apostas";
+$user = "root";
+$password = "1234"; 
+
+try {
+    $conn = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $password);
+    
+    
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Ligação à base de dados feita com sucesso!";
+
+} catch(PDOException $e) {
+    echo "Erro na ligação: " . $e->getMessage();
+}
+
